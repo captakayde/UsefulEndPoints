86c2da6c776efc7978b95c00342deab533e7c441
AkayDe <akdesai8916@gmail.com>
2017-12-22 19:02:10 -0500

    Initial Commit
    Boilerplate generater from spring boot website

---
+++ currencymonitor/src/main/java/com/alexa/currencymonitor/CurrencymonitorApplication.java
@@ -0,0 +1,12 @@
+package com.alexa.currencymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencymonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencymonitorApplication.class, args);
+	}
+}
